d9952bb81ec1ced8f32b2dbbf5f5c60f19d659b2
mr-chris-weaver <156230957+mr-chris-weaver@users.noreply.github.com>
2024-02-23 14:40:46 -0600

    Get all secrets in a folder in Secrets Safe
    This PowerShell script will return all secrets in a folder named Demo that the API user has access to. Modify as necessary to meet your needs.

---
+++ Get_All_Secrets_In_Folder.ps1
@@ -0,0 +1,21 @@
+﻿#import basic functions for calls to Secrets Safe/Password Safe API
+Import-module .\REALLYGENERIC.ps1
+
+#authenticate against the API
+PSafe-SignAppin
+
+#grab all the secrets in the Demo folder that the API user has access to excluding the actual password
+$secrets= psafe-get "Secrets-Safe/Folders/fe864bac-2b7f-4c97-efeb-08dc29723137/secrets" |Select -ExpandProperty Id
+#declaring it as an array variable
+$output=@()
+
+#grab all the secret information from the above $secrets including the password
+foreach($secret in $secrets){
+
+    $output+= psafe-get  "Secrets-Safe/Secrets/$secret"
+}
+
+#output to screen as an auto-sized table
+$output |ft -AutoSize
+
+PSafe-Post "Auth/Signout"
---
+++ REALLYGENERIC.ps1
@@ -0,0 +1,240 @@
+#requires -version 3.0
+
+
+####Powershell: Various Generic Samples (see $sampleType below).
+
+####BeyondInsight and Password Safe API: 6.4.4+
+####Workflow: Sign in, Execute sample type, Sign out
+####Permissions: Dependent on sample type
+
+#### Script Version: 1.2
+#### Modified: 29-Jun-2018
+
+
+cls;
+
+
+#Secure Connection
+$baseUrl = Get-ChildItem Env:\PS_instance | select -ExpandProperty Value
+#The Application API Key generated in BeyondInsight
+$apiKey = Get-ChildItem Env:\Auth_key | select -ExpandProperty Value
+#Username of BeyondInsight user granted permission to the API Key
+$runAsUser = Get-ChildItem Env:\API_user| select -ExpandProperty Value
+
+#Password if required by the API registration
+#$runAsUserPassword = "un1qu3";
+
+
+
+#Client Certificate Parameters:
+#Type of certificate. Possible values:
+#None
+#BICertificate
+#SmartCardLogon
+$clientCertificateType = "None";
+
+#Verbose logging?
+$verbose = $True;
+
+
+
+#region Trust All Certificates
+#Uncomment the following block if you want to trust an unsecure connection.
+
+#The Invoke-RestMethod CmdLet does not currently have an option for ignoring SSL warnings (i.e self-signed CA certificates).
+#This policy is a temporary workaround to allow that for development purposes.
+#Warning: If using this policy, be absolutely sure the host is secure.
+add-type "
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(ServicePoint srvPoint, X509Certificate certificate, WebRequest request, int certificateProblem)
+    {
+        return true;
+    }
+}
+";
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy;
+
+#endregion
+
+
+#Script-level Client Certificate
+[System.Security.Cryptography.X509Certificates.X509Certificate2]$script:authCert = $null;
+
+
+#region Functions
+
+#Builds a full URI for the given API
+function PSafe-BuildUri([string]$api)
+{
+    "{0}{1}" -f $baseUrl, $api;
+}
+
+
+#Builds and returns the headers for the request
+function PSafe-BuildHeaders()
+{
+    #Build the Authorization header
+    if ( $script:runAsUserPassword -eq $null )
+    { @{ Authorization="PS-Auth key=${script:apiKey}; runas=${script:runAsUser};"; }; }
+    else
+    { @{ Authorization="PS-Auth key=${script:apiKey}; runas=${script:runAsUser}; pwd=[${script:runAsUserPassword}];"; }; }
+}
+
+#Calls the SignAppin API
+function PSafe-SignAppin()
+{
+    $method = "POST";
+    $uri = PSafe-BuildUri "Auth/SignAppin";
+    $headers = PSafe-BuildHeaders;
+
+    try
+    {
+        if ($script:authCert -eq $null)
+        {
+            $result = Invoke-RestMethod -Uri $uri -Method $method -Headers $headers -SessionVariable script:session;
+            $result;
+        }
+        else
+        {
+            $result = Invoke-RestMethod -Uri $uri -Method $method -Headers $headers -SessionVariable script:session -Certificate $script:authCert;
+            $result;
+        }
+    }
+    catch [System.Net.WebException]
+    {
+        #401 with WWW-Authenticate-2FA header expected for two-factor authentication challenge
+        if($_.Exception.Response.StatusCode -eq 401 -and $_.Exception.Response.Headers.Contains("WWW-Authenticate-2FA") -eq $true)
+        {
+            $challengeMessage = $_.Exception.Response.Headers["WWW-Authenticate-2FA"];
+            $challengeResponse = Read-Host $challengeMessage;
+            PSafe-SignAppinChallenge $challengeResponse;
+        }
+        else
+        {
+            throw;
+        }
+    }
+
+}
+
+#Calls the SignAppin API with an Authentication challenge
+#Note: Should only be called after an initial attempt at Auth/SignAppin since it uses the existing Web Session
+function PSafe-SignAppinChallenge($challengeResponse)
+{
+    $method = "POST";
+    $uri = PSafe-BuildUri "Auth/SignAppin";
+    $headers = PSafe-BuildHeaders;
+
+    # add challenge to the Auth header
+    $headers["Authorization"] = "$($headers["Authorization"]) challenge=$($challengeResponse);"; 
+
+    if ($script:authCert -eq $null)
+    {
+        $result = Invoke-RestMethod -Uri $uri -Method $method -Headers $headers -WebSession $script:session;
+        $result;
+    }
+    else
+    {
+        $result = Invoke-RestMethod -Uri $uri -Method $method -Headers $headers -WebSession $script:session -Certificate $script:authCert;
+        $result;
+    }
+}
+
+#Calls the given API
+function PSafe-RestMethod([string]$method, [string]$api, $body)
+{
+    $uri = PSafe-BuildUri $api;
+    $headers = PSafe-BuildHeaders;
+
+    if ($script:authCert -eq $null)
+    {
+        $result = Invoke-RestMethod -Uri $uri -Method $method -WebSession $script:session -Headers $headers -Body $body;
+        $result;
+    }
+    else
+    {
+        $result = Invoke-RestMethod -Uri $uri -Method $method -WebSession $script:session -Headers $headers -Body $body -Certificate $script:authCert;
+        $result;
+    }
+}
+
+#Calls the given API with a custom Content Type
+function PSafe-RestMethod([string]$method, [string]$api, $body, $contentType)
+{
+    $uri = PSafe-BuildUri $api;
+    $headers = PSafe-BuildHeaders;
+
+    if ($script:authCert -eq $null)
+    {
+        $result = Invoke-RestMethod -Uri $uri -Method $method -WebSession $script:session -ContentType $contentType -Headers $headers -Body $body;
+        $result;
+    }
+    else
+    {
+        $result = Invoke-RestMethod -Uri $uri -Method $method -WebSession $script:session -ContentType $contentType -Headers $headers -Body $body -Certificate $script:authCert;
+        $result;
+    }
+}
+
+#Calls a POST API
+function PSafe-Post([string]$api, $body)
+{
+    PSafe-RestMethod "POST" $api $body;
+}
+
+#Calls a POST API with the given ContentType
+function PSafe-Post([string]$api, $body, $contentType)
+{
+    PSafe-RestMethod "POST" $api $body $contentType;
+}
+
+#Calls a GET API
+function PSafe-Get([string]$api, $body)
+{
+    PSafe-RestMethod "GET" $api $body;
+}
+
+#Calls a PUT API
+function PSafe-Put([string]$api, $body)
+{
+    PSafe-RestMethod "PUT" $api $body "application/json";
+}
+
+#Calls a DELETE API
+function PSafe-Delete([string]$api, $body)
+{
+    PSafe-RestMethod "DELETE" $api $body;
+}
+
+#Json Wrapper - The Powershell Cmd-lets have an intrinsic maximum json length that cannot be modified
+#  Note: This accepts piped input.
+function From-Json()
+{
+    [void][System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions"); 
+    $jsonserial = New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer;
+    $jsonserial.MaxJsonLength = [int]::MaxValue;
+    $jsonserial.DeserializeObject($input);
+}
+
+#Displays the given list of items
+function Display-Items($items)
+{
+    $cnt = $items.Length;
+    if ($cnt -eq $null)
+    { 
+        $cnt = "1"; 
+    }
+
+    "..$($cnt) returned:";
+    foreach($item in $items) 
+    { 
+        "$($item)"; 
+    }
+}
+
+#endregion
+
+#PSafe-SignAppin
+#psafe-post "UserGroups" @{"GroupType"="ActiveDirectory";"GroupName"="req_BPM";"description"="req_BPM";"domainName"="packet.farm";"SmartRuleAccess"=@{"SmartRuleID"=10038;"AccessLevelID"=1}}
